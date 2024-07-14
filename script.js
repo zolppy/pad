ebb92a7db5195e68138ed8c693949dc794db41cb
Gabriel Cavalcante <gabriel.lcifba@gmail.com>
2024-07-14 11:11:26 -0300

    refactor: otimização de js e readequação de outros códigos

--- script.js
+++ script.js
@@ -1,127 +1,57 @@
-const botaoDo = document.querySelector(".do");
-const botaoRe = document.querySelector(".re");
-const botaoMi = document.querySelector(".mi");
-const botaoFa = document.querySelector(".fa");
-const botaoSol = document.querySelector(".sol");
-const botaoLa = document.querySelector(".la");
-const botaoSi = document.querySelector(".si");
-const botaoDo2 = document.querySelector(".do2");
-const botaoRe2 = document.querySelector(".re2");
+const notes = document.querySelectorAll(".tile");
+const body = document.querySelector("body");
 const okButton = document.querySelector(".ok");
-const corpo = document.querySelector("body");
 
-botaoDo.addEventListener("click", () => {
-  const audio = new Audio("songs/1do.mp3");
-  audio.play();
+const paths = [
+  "songs/do.mp3",
+  "songs/re.mp3",
+  "songs/mi.mp3",
+  "songs/fa.mp3",
+  "songs/sol.mp3",
+  "songs/la.mp3",
+  "songs/si.mp3",
+  "songs/do2.mp3",
+  "songs/re2.mp3",
+];
+
+const colors = [
+  "blue",
+  "red",
+  "blue",
+  "red",
+  "green",
+  "red",
+  "blue",
+  "red",
+  "blue",
+];
+
+const keys = ["7", "8", "9", "4", "5", "6", "1", "2", "3"];
+
+notes.forEach((note, index) => {
+  note.addEventListener("click", () => {
+    new Audio(paths[index]).play();
+    effect(note, colors[index]);
+  });
 });
 
-botaoRe.addEventListener("click", () => {
-  const audio = new Audio("songs/1re.mp3");
-  audio.play();
-});
+const effect = (note, color) => {
+  note.style.backgroundColor = color;
 
-botaoMi.addEventListener("click", () => {
-  const audio = new Audio("songs/1mi.mp3");
-  audio.play();
-});
+  setTimeout(() => {
+    note.style.backgroundColor = "inherit";
+  }, 200);
+};
 
-botaoFa.addEventListener("click", () => {
-  const audio = new Audio("songs/1fa.mp3");
-  audio.play();
-});
+body.addEventListener("keypress", (event) => {
+  const key = event.key;
 
-botaoSol.addEventListener("click", () => {
-  const audio = new Audio("songs/1sol.mp3");
-  audio.play();
-});
-
-botaoLa.addEventListener("click", () => {
-  const audio = new Audio("songs/1la.mp3");
-  audio.play();
-});
-
-botaoSi.addEventListener("click", () => {
-  const audio = new Audio("songs/1si.mp3");
-  audio.play();
-});
-
-botaoDo2.addEventListener("click", () => {
-  const audio = new Audio("songs/2do.mp3");
-  audio.play();
-});
-
-botaoRe2.addEventListener("click", () => {
-  const audio = new Audio("songs/2re.mp3");
-  audio.play();
-});
-
-corpo.addEventListener("keypress", (event) => {
-  const tecla = event.key;
-
-  switch (tecla) {
-    case "7":
-      botaoDo.click();
-      botaoDo.style.backgroundColor = "blue";
-      setTimeout(() => {
-        botaoDo.style.backgroundColor = "inherit";
-      }, 200);
-      break;
-    case "8":
-      botaoRe.click();
-      botaoRe.style.backgroundColor = "red";
-      setTimeout(() => {
-        botaoRe.style.backgroundColor = "inherit";
-      }, 200);
-      break;
-    case "9":
-      botaoMi.click();
-      botaoMi.style.backgroundColor = "blue";
-      setTimeout(() => {
-        botaoMi.style.backgroundColor = "inherit";
-      }, 200);
-      break;
-    case "4":
-      botaoFa.click();
-      botaoFa.style.backgroundColor = "red";
-      setTimeout(() => {
-        botaoFa.style.backgroundColor = "inherit";
-      }, 200);
-      break;
-    case "5":
-      botaoSol.click();
-      botaoSol.style.backgroundColor = "green";
-      setTimeout(() => {
-        botaoSol.style.backgroundColor = "inherit";
-      }, 200);
-      break;
-    case "6":
-      botaoLa.click();
-      botaoLa.style.backgroundColor = "red";
-      setTimeout(() => {
-        botaoLa.style.backgroundColor = "inherit";
-      }, 200);
-      break;
-    case "1":
-      botaoSi.click();
-      botaoSi.style.backgroundColor = "blue";
-      setTimeout(() => {
-        botaoSi.style.backgroundColor = "inherit";
-      }, 200);
-      break;
-    case "2":
-      botaoDo2.click();
-      botaoDo2.style.backgroundColor = "red";
-      setTimeout(() => {
-        botaoDo2.style.backgroundColor = "inherit";
-      }, 200);
-      break;
-    case "3":
-      botaoRe2.click();
-      botaoRe2.style.backgroundColor = "blue";
-      setTimeout(() => {
-        botaoRe2.style.backgroundColor = "inherit";
-      }, 200);
-  }
+  keys.forEach((item, index) => {
+    if (item === key) {
+      notes[index].click();
+      effect(notes[index]);
+    }
+  });
 });
 
 okButton.addEventListener("click", function () {
